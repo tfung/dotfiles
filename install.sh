d216bd1a945bef3bcbdef99f46b56c8a16063689
Thomas Fung <thomas.fung1@gmail.com>
2022-02-08 11:09:52 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
+# Install Oh My Zsh
+export ZSH="$HOME/.zsh.d/oh-my-zsh"; sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
 ln -sf ~/dotfiles/vimrc ~/.vimrc
 ln -sf ~/dotfiles/zshrc ~/.zshrc
 
